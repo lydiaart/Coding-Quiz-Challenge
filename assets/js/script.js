722197447a5978c3a199fa866259b5608558d079
Lydia <lydia_art@yahoo.com>
2021-10-14 20:15:32 -0500

    added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-var
+var data = [{
+    question:"Commonly used data types D NOT include:",
+    options:["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    correctAnswer:3,
+},
+{
+    question:"The condition in an if/else statement is enclosed with____.",
+    options:["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    correctAnswer:2,
+},
+{
+    question:"Arrays in JavaScript can be used to store____.",
+    options:["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    correctAnswer:4,
+},
+{
+    question:"String values must be enclosed within____when being assigned to variables.",
+    options:["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    correctAnswer:3,
+},
+{
+    question:"A very useful tool used during development and debugging for printing content to the debugger is:",
+    options:["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+    correctAnswer:4,
+}
+]
